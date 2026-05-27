c13cfa218427d1e991f8edf83751cc50e848ca67
Nitish <24f3002262ds.study.iitm.ac.in>
2026-05-27 15:44:36 +0530

    fix api routes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'https://tattoobymanish-api.onrender.com';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://tattoobymanish-api.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
